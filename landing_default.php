7b924295c6c8791c4f6dbc24e456ba4387024808
generic-guy-2005 <jevirahmat0@gmail.com>
2025-11-27 21:59:44 +0700

    Different Role Landing Page Established

--- landing_default.php
+++ landing_default.php
@@ -1,3 +1,6 @@
+<?php
+if(!$_SESSION['role'] || $_SESSION['role'] === 'admin') {
+?>
 <section class="bg-blue-50 bg-local bg-[url(assets/breathtaking-view-snowy-mountains-cloudy-sky-patagonia-chile.jpg)] relative overflow-hidden bg-cover bg-center m-8 rounded-xl">
     <div class="flex flex-col md:flex-row items-start justify-between py-20 px-20">
         <div class="glassmorphism-element md:w-1/2 text-center md:text-left mb-8 md:mb-0 p-6">
@@ -131,7 +134,7 @@
 </section>
 
 <?php
-include 'course/view_course.php';
+include 'course/course_view.php';
 ?>
 
 <section class="bg-gradient-to-r from-blue-600 to-indigo-700 py-12">
@@ -148,3 +151,46 @@ include 'course/view_course.php';
         </div>
     </div>
 </section>
+
+<?php
+} elseif($_SESSION['role'] === 'student') {
+?>
+
+<?php
+    $images = [
+        "assets/ales-krivec-4miBe6zg5r0-unsplash.jpg",
+        "assets/bailey-zindel-NRQV-hBF10M-unsplash.jpg",
+        "assets/kalen-emsley-Bkci_8qcdvQ-unsplash.jpg",
+        "assets/mark-harpur-K2s_YE031CA-unsplash.jpg",
+        "assets/simon-twukN12EN7c-unsplash.jpg"
+    ];
+
+    $background = $images[array_rand($images)];
+?>
+
+<section class="bg-blue-50 bg-local relative overflow-hidden bg-cover bg-center m-8 rounded-xl glassmorphism-element"
+style="background-image: url('<?= $background ?>'); background-size:cover">
+    <div class="flex flex-col md:flex-row items-start justify-between py-20 px-20">
+        <div class="md:w-1/2 text-center md:text-left mb-8 md:mb-0 p-6">
+            <h1 class="text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight mt-2 mb-4">
+                Welcome Back! <span class="bg-linear-to-r from-cyan-500 to-blue-800 bg-clip-text text-5xl font-extrabold text-transparent"><?= $_SESSION['username'] ?></span>
+            </h1>
+        </div>
+    </div>
+</section>
+
+<section>
+    <p>Enrolled Course Section</p>
+</section>
+
+<section>
+    <p>Recommendation Course Section</p>
+</section>
+
+<section>
+    <p>Statistic Section</p>
+</section>
+
+<?php
+}
+?>
